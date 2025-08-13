f7fdf4a596eb0f1feb156e1cf122b43da92373a0
develkkm <kkm8905508@daum.net>
2025-08-13 11:47:02 +0900

    [Bronze V] Title: 시험 성적, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException{
+        String str = br.readLine();
+        st = new StringTokenizer(str);
+        int score = Integer.parseInt(st.nextToken());
+
+        if(score>=90) sb.append("A");
+        else if(score>=80) sb.append("B");
+        else if(score>=70) sb.append("C");
+        else if(score>=60) sb.append("D");
+        else sb.append("F");
+
+        System.out.println(sb);
+
+    }
+}
