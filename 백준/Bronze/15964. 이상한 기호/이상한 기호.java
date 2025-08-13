3f0be8af5ed6da4a51aefa782c5813798242f6e9
develkkm <kkm8905508@daum.net>
2025-08-13 15:25:33 +0900

    [Bronze V] Title: 이상한 기호, Time: 108 ms, Memory: 14308 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException{
+        st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+        System.out.println((a+b) * (a-b));
+    }
+}
