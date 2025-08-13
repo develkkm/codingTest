670630feb1fda151c0c70046c048def43722dbd2
develkkm <kkm8905508@daum.net>
2025-08-13 11:38:54 +0900

    [Bronze V] Title: 꼬마 정민, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException{
+        String str = br.readLine();
+        st = new StringTokenizer(str);
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+
+        System.out.println(a+b+c);
+    }
+}
