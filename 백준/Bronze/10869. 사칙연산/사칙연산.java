a08a35627a29c946bd34ab56cb1e16e6d04c9405
develkkm <kkm8905508@daum.net>
2025-08-13 11:35:20 +0900

    [Bronze V] Title: 사칙연산, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException{
+        String str = br.readLine();
+        st = new StringTokenizer(str);
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        sb.append(a+b).append("\n");
+        sb.append(a-b).append("\n");
+        sb.append(a*b).append("\n");
+        sb.append(a/b).append("\n");
+        sb.append(a%b);
+
+        System.out.println(sb);
+    }
+}
