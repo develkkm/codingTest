bbfa9f270856c9274fe35dacba377145332eb047
develkkm <kkm8905508@daum.net>
2025-08-13 11:32:03 +0900

    [Bronze V] Title: A-B, Time: 100 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException{
+        String str = br.readLine();
+        st = new StringTokenizer(str);
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a-b);
+    }
+}
