e7a88bc5455a3083c9f5b8debb777c6042a6f250
develkkm <kkm8905508@daum.net>
2025-08-14 11:21:15 +0900

    [Bronze V] Title: 나머지, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException{
+        st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        sb.append((A+B)%C).append("\n");
+        sb.append(((A%C) + (B%C))%C).append("\n");
+        sb.append((A*B)%C).append("\n");
+        sb.append(((A%C) * (B%C))%C);
+
+        System.out.println(sb);
+    }
+}
