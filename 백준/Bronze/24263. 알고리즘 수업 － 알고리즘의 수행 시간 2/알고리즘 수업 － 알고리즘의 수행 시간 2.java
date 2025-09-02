788281bac57b5437e4ace5afab6e6180241fc241
develkkm <kkm8905508@daum.net>
2025-09-02 11:00:23 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException{
+        sb.append(Integer.parseInt(br.readLine())).append("\n").append(1);
+
+        System.out.println(sb);
+    }
+}
