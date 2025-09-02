d3cc90aa761f2c757497bc53581c15a200c7f56e
develkkm <kkm8905508@daum.net>
2025-09-02 10:23:17 +0900

    [Bronze III] Title: 네 번째 점, Time: 124 ms, Memory: 15996 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException{
+        st = new StringTokenizer(br.readLine());
+        int x1 = Integer.parseInt(st.nextToken());
+        int y1 = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int x2 = Integer.parseInt(st.nextToken());
+        int y2 = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int x3 = Integer.parseInt(st.nextToken());
+        int y3 = Integer.parseInt(st.nextToken());
+
+        int x4 = 0;
+        int y4 = 0;
+
+        if(x1 == x2) x4 = x3;
+        else if(x1 == x3) x4 = x2;
+        else x4 = x1;
+
+        if(y1 == y2) y4 = y3;
+        else if(y1 == y3) y4 = y2;
+        else y4 = y1;
+
+        System.out.println(x4 +" "+y4);
+    }
+}
