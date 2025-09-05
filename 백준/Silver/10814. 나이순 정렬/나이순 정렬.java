3e5c8f1c37d74b566f46263ff4c1d58fa55fca08
develkkm <kkm8905508@daum.net>
2025-09-05 13:07:32 +0900

    [Silver V] Title: 나이순 정렬, Time: 700 ms, Memory: 61408 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException{
+        int n = Integer.parseInt(br.readLine());
+        String[][] arr = new String[n][2];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = st.nextToken();
+            arr[i][1] = st.nextToken();
+        }
+
+        Arrays.sort(arr, Comparator.comparing((String[] s) -> Integer.parseInt(s[0])));
+
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
