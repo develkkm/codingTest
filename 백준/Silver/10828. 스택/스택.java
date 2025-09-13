f008ff5abc06eab59cfe941d072520a52a7ed5c2
develkkm <kkm8905508@daum.net>
2025-09-13 12:09:10 +0900

    [Silver IV] Title: 스택, Time: 120 ms, Memory: 17072 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException{
+        int t = Integer.parseInt(br.readLine());
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        while(t-->0){
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+
+            switch(str){
+                case "push":
+                    dq.addLast(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    if(dq.isEmpty()) sb.append(-1);
+                    else sb.append(dq.removeLast());
+                    sb.append("\n");
+                    break;
+                case "size":
+                    sb.append(dq.size());
+                    sb.append("\n");
+                    break;
+                case "empty":
+                    if(dq.isEmpty()) sb.append(1);
+                    else sb.append(0);
+                    sb.append("\n");
+                    break;
+                case "top":
+                    if(dq.isEmpty()) sb.append(-1);
+                    else sb.append(dq.getLast());
+                    sb.append("\n");
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+}
