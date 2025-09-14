07d6e26e7367c0c590d6e63b21533e63e2f2ee91
develkkm <kkm8905508@daum.net>
2025-09-14 10:53:39 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 64 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException{
+        String str = br.readLine();
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::"+ str +"::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
