9ae1c9a12a946156a8c42303d4dc8bb01261e6fc
develkkm <kkm8905508@daum.net>
2025-11-08 10:20:10 +0900

    [Silver V] Title: 배열 합치기, Time: 1204 ms, Memory: 340160 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException{
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n+m];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            arr[i+n] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for(int num : arr){
+            sb.append(num).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
