846554eb182ea5478478a3e8253f7f1bacce218a
develkkm <kkm8905508@daum.net>
2025-11-11 10:04:33 +0900

    [Silver III] Title: 수열, Time: 172 ms, Memory: 21980 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < m; i++) {
+            sum += arr[i];
+        }
+
+        int max = sum;
+        for (int i = m; i <n ; i++) {
+            sum -= arr[i - m];
+            sum += arr[i];
+            max = Math.max(sum,max);
+        }
+
+        System.out.println(max);
+
+    }
+}
