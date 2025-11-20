2b945a98836f3b57eba2a15686240a3d74b5acec
develkkm <kkm8905508@daum.net>
2025-11-20 14:52:33 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 836 ms, Memory: 90796 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static ArrayList<Integer>[] adj;
+    static boolean[] visited;
+    static int[] order;
+    static int cnt = 1;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int sn = Integer.parseInt(st.nextToken());
+
+        adj = new ArrayList[v+1];
+        for (int i = 0; i < v+1; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        visited = new boolean[v+1];
+        order = new int[v+1];
+
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            adj[n].add(m);
+            adj[m].add(n);
+        }
+
+        for (ArrayList<Integer> list : adj) {
+            list.sort(Comparator.reverseOrder());
+        }
+
+        dfs(sn);
+
+        for (int i = 1; i < v+1; i++) {
+            sb.append(order[i]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void dfs(int cur){
+        visited[cur] = true;
+        order[cur] = cnt++;
+
+        for(int next : adj[cur]){
+            if(!visited[next])
+                dfs(next);
+        }
+    }
+}
