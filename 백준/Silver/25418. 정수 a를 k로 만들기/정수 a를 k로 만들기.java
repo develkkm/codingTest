3baa2fd1764478d99492434c5f3c6ea7380ecf2a
develkkm <kkm8905508@daum.net>
2025-12-28 13:18:35 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 124 ms, Memory: 53220 KB -BaekjoonHub

--- 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.java
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.java
@@ -1,37 +1,37 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
-
+class Main{
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         st = new StringTokenizer(br.readLine());
         int a = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        ArrayDeque<int[]> queue = new ArrayDeque<>();
+
+        Deque<int []> dq = new ArrayDeque<>();
         boolean[] visited = new boolean[k+1];
-
-        queue.addLast(new int[] {a,0});
-
-        while(!queue.isEmpty()){
-            int[] arr = queue.removeFirst();
+        dq.addLast(new int[]{a,0});
+        int result = 0;
+        while(true){
+            int[] arr = dq.removeFirst();
+            int num = arr[0];
+            int cnt = arr[1];
 
             if (arr[0] >= visited.length || visited[arr[0]]) continue;
+            visited[num] = true;
 
-            visited[arr[0]] = true;
-
-            if(arr[0] == k){
-                sb.append(arr[1]);
+            if(num == k) {
+                result = cnt;
                 break;
             }
 
-            queue.addLast(new int[] {arr[0]+1,arr[1]+1});
-            queue.addLast(new int[] {arr[0]*2,arr[1]+1});
+            dq.addLast(new int[]{num+1,cnt+1});
+            dq.addLast(new int[]{num*2,cnt+1});
         }
 
-        System.out.print(sb);
+        System.out.println(result);
     }
 }
